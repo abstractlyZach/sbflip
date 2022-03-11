2882d26b388d40eb6aeb60f72d3e1c9e5a0b56d0
abstractlyZach <zach3lee@gmail.com>
2022-03-11 13:37:41 -0800

    Create basic command line app

--- src/sbflip/__main__.py
+++ src/sbflip/__main__.py
@@ -1,11 +1,23 @@
 """Command-line interface."""
+import time
+
 import click
+
+from sbflip import tiles
 
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """sbflip."""
+    flipboard = tiles.Flipboard(80, " abcdefghijklmnopqrstuvwxyz")
+    flipboard.set_message("hi welcome to my flipboard")
+    flipboard.set_message("this is pretty cool i think")
+
+    while True:
+        print(flipboard.text, end="\r")
+        flipboard.tick()
+        time.sleep(0.25)
 
 
 if __name__ == "__main__":
